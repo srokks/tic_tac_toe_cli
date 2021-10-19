0c5711b04f86cf7ae00c32e2a84ac864ee1b89ee
srokks <srokks@gmail.com>
2021-10-19 12:11:20 +0200

    added insert move funct

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -5,11 +5,8 @@ def read_input():
     return input_
 
 
-def gen_board(input_):
-    input_ = [x for x in input_]
-    board = []
-    for i in range(0, 9, 3):
-        board.append(input_[i:i + 3])
+def gen_board():
+    board = [['_' for i in range(3)] for j in range(3)]
     return board
 
 
@@ -19,10 +16,28 @@ def print_board(board):
         print('| ' + ' '.join(el) + ' |')
     print('---------')
 
+def take_move():
+    'Takes input validate it and return as list of pos'
+    while True:
+        input_ = input('Enter the coordinates:')
+        input_ = input_.split()
+        if input_[0].isdigit() and input_[1].isdigit():
+            input_ = [int(x)-1 for x in input_]
+            if all([x in range(3) for x in input_]):
 
+                if ('X' in board[input_[0]][input_[1]]) or ("O" in board[input_[0]][input_[1]]):
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    return input_
+            else:
+                print('Coordinates should be from 1 to 3!')
+        else:
+            print('You should enter numbers!')
+def insert_move(board,pos,identyfier):
+    board[pos[0]][pos[1]] = identyfier
 def check_state(board):
     win_board = []
-
+    input_ = ''.join([y for x in board for y in x])
     def get_winning(el):
         """Takes el as list and count if """
         if el.count('X') == 3:
@@ -42,28 +57,43 @@ def check_state(board):
     anti_diagonal = [board[2][0], board[1][1], board[0][2]]  # gens anti diagonal
     win = get_winning(anti_diagonal)# checks if theres win in anti diagonal
     win_board.append(win) if win is not None else None # appends win board if theres win
-    # print(win_board) # DEBUG:prints win board
-    win_state = '' # holds state of win [DRAW,X WINS,O WINS]
-    if win_board.count('X') == win_board.count('O'):  # checks if same both sides has same wins
-        win_state = 'Draw'  #
-    elif win_board.count('X') > win_board.count('O'): # checks if x has more wins than o
-        win_state = 'X wins'
-    elif win_board.count('X') < win_board.count('O'):# checks if o has more wins than x
-        win_state = 'O wins'
+    print('win obard',win_board) # DEBUG:prints win board
+    win_state = False # holds state of win [DRAW,X WINS,O WINS]
+    if len(win_board)!=0:
+        if win_board.count('X') == win_board.count('O'):  # checks if same both sides has same wins
+            win_state = 'Draw'  #
+        elif win_board.count('X') > win_board.count('O'): # checks if x has more wins than o
+            win_state = 'X wins'
+        elif win_board.count('X') < win_board.count('O'):# checks if o has more wins than x
+            win_state = 'O wins'
     # saves state of game its absolute of dif of count x and o in input
     state = abs([x for x in input_].count('X') - [x for x in input_].count('O'))
-    # print(state)  #  Debug
+    print('state:',state)  #  Debug
     if state >= 2: # if state >2
-        win_state = 'Impossible'
-    if win_state == 'Draw' and state >= 1 and len(win_board)>0:
-        win_state = 'Impossible'
-    elif state == 0 and len(win_board)==0:
-        win_state = 'Game not finished'
-    print(win_state)
-input_  = read_input()
-# input_ = 'XO_XO_XOX' # Debug
-board = gen_board(input_)
-print_board(board)
-check_state(board)
+        win_state = '1Impossible'
+    if len(win_board)>=  and state == 1:
+        win_state = '2Impossible'
+    elif len(win_board)==0 and state < 1:
+        pass
+    return win_state
+# input_  = read_input()
+# input_ = 'X_X_O____' # Debug
+board = gen_board()
+# board = [['X','O','X'],['X','O','X'],['O','X','O']]
+# print(check_state(board))
+# print_board(board)
+while True:
+    tokens = ['X','O']
+    for el in tokens:
+        print_board(board)
+        next_move = take_move()
+        insert_move(board,next_move,el)
+        win_state = check_state(board)
+        if win_state:
+            print_board(board)
+            print(win_state)
+            break
+    if win_state:
+        break
 
 
